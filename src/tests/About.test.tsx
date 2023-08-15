810bccc6d836d5c3a6c6dd7988c1646fa5afd4a0
ElisaBorba <elisaborbaa@gmail.com>
2023-08-15 00:57:42 -0300

    req 2

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import { renderWithRouter } from './utils/renderWithRouter';
+
+describe('Teste se na página about contém as informações sobre a Pokédex.', () => {
+  it('A página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<App />, { route: '/about' });
+    const headingEl = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(headingEl).toBeInTheDocument();
+  });
+
+  it('A página contém uma imagem com o atributo \'src\': https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
+    renderWithRouter(<App />, { route: '/about' });
+    const imageEl = screen.getByAltText(/pokédex/i);
+
+    expect(imageEl).toBeInTheDocument();
+    expect(imageEl.getAttribute('src')).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
