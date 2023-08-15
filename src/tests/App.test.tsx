cec1d2d752c859ce42dd08ea1334e32e487629c8
ElisaBorba <elisaborbaa@gmail.com>
2023-08-15 00:24:57 -0300

    req 1

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import { renderWithRouter } from './utils/renderWithRouter';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação:', () => {
+  it('É exibido na tela um link com o texto: Home', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+  });
+  it('É exibido na tela um link com o texto: About', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+  });
+  it('É exibido na tela um link com o texto: Favorite Pokémon', () => {
+    renderWithRouter(<App />);
+    const favoritesLink = screen.getByRole('link', { name: /favorite pokémon/i });
+    expect(favoritesLink).toBeInTheDocument();
+  });
+});
