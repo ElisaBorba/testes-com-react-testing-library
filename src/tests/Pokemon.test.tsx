4784de368530213949c06bf1986f60a5a928a26e
ElisaBorba <elisaborbaa@gmail.com>
2023-08-18 03:25:26 -0300

    refatora

--- src/tests/Pokemon.test.tsx
+++ src/tests/Pokemon.test.tsx
@@ -1,44 +1,25 @@
 import { screen } from '@testing-library/react';
 import App from '../App';
 import { renderWithRouter } from './utils/renderWithRouter';
-// import pokemonList from '../data';
 
 describe('Testa o componente <Pokemon.tsx />', async () => {
   it('A imagem do Pokémon tem o src correto e o alt: <name> sprite', async () => {
-    const { user } = renderWithRouter(<App />);
+    renderWithRouter(<App />);
 
     const pikachuName = screen.getByText(/pikachu/i);
     const pikachuImage = screen.getByRole('img', { name: /pikachu sprite/i });
     const pikachuWeight = screen.getByText(/average weight: 6\.0 kg/i);
-
     const pokemonType = screen.getByTestId('pokemon-type');
+    const pikachuLink = screen.getByRole('link', { name: /more details/i });
 
     expect(pokemonType).toBeInTheDocument();
     expect(pokemonType.innerHTML).toBe('Electric');
-
-    const pikachuLink = screen.getByRole('link', { name: /more details/i });
-
     expect(pikachuName).toBeInTheDocument();
     expect(pikachuImage).toBeInTheDocument();
     expect(pikachuWeight).toBeInTheDocument();
     expect(pikachuImage.getAttribute('src')).toBe('https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png');
     expect(pikachuImage.getAttribute('alt')).toBe('Pikachu sprite');
     expect(pikachuLink).toBeInTheDocument();
-
-    const nextBtn = screen.getByRole('button', { name: /próximo pokémon/i });
-
-    await user.click(nextBtn);
-    await user.click(nextBtn);
-
-    const caterpieName = screen.getByText(/caterpie/i);
-    const caterpieImage = screen.getByRole('img', { name: /caterpie sprite/i });
-    const caterpieWeight = screen.getByText(/average weight: 2\.9 kg/i);
-
-    expect(caterpieName).toBeInTheDocument();
-    expect(caterpieImage).toBeInTheDocument();
-    expect(caterpieWeight).toBeInTheDocument();
-    expect(caterpieImage.getAttribute('src')).toBe('https://archives.bulbagarden.net/media/upload/8/83/Spr_5b_010.png');
-    expect(caterpieImage.getAttribute('alt')).toBe('Caterpie sprite');
   });
 
   it('O card do Pokémon indicado na Pokédex contém um link de navegação para exibir detalhes desse Pokémon. O link deve ter a URL /pokemon/<id>, em que <id> é o id do Pokémon exibido', async () => {
